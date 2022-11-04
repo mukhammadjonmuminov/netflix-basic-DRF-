e97779231fad22433e3befc1be586e6154b70822
mukhammadjonmuminov <muhammadjonmominov77@gmail.com>
2022-11-04 12:15:17 +0500

    Updated All files

---
+++ Netflix/Netflix/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Netflix Aplication APi",
+      default_version="v1",
+      description="Swager docs for REST API",
+      contact=openapi.Contact("Mukhammadjon Muminov <gmail@muhammadjonmominov77.com>")
+   ),
+   public=True,
+   permission_classes=[AllowAny, ]
+)
+
+urlpatterns = [
+   path('admin/', admin.site.urls),
+   path('', include('home.urls')),
+   path('docs/', schema_view.with_ui('swagger'), name="swagger-docs"),
+   path('redoc/', schema_view.with_ui('redoc'), name="redoc"),
+]
